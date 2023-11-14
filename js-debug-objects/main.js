8a81ea5bdb3238a894fd1f0aae56a2adde263e47
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-11-14 18:50:33 +0100

    Add: js-debug-obj

--- js-debug-objects/main.js
+++ js-debug-objects/main.js
@@ -56,7 +56,7 @@ const cars = [
         manufacturer: 'Seat',
         model: 'Ibiza',
         type: 'metano'
-    }
+    },
     {
         manufacturer: 'Audi',
         model: 'R8',
@@ -64,15 +64,11 @@ const cars = [
     },
 ];
 
-const gasolineCars = cars.filter( (auto) >= auto.type === 'benzina');
+const gasolineCars = cars.filter( (auto) => auto.type.toLowerCase() === 'benzina');
 
-const dieselCars = cars.filter( (auto) => {
-    auto.type === 'diesel';
-});
+const dieselCars = cars.filter((auto) => auto.type.toLowerCase() === 'diesel');
 
-const otherCars = cars.filter( (auto) => {
-    return auto.type !== 'benzina' || auto.type !== 'diesel';
-});
+const otherCars = cars.filter((auto) => auto.type.toLowerCase() !== 'benzina' && auto.type !== 'diesel');
 
 console.log('Auto a benzina');
 console.log('*******************************');
@@ -84,3 +80,15 @@ console.log(dieselCars);
 
 console.log('Tutte le altre auto');
 console.log(otherCars);
+
+
+/* La funzione filtra l'array originario di oggetti 'cars', creando diversi array vuoti in base al valore della proprietà 'type', che può essere 'benzina', 'diesel' o altri tipi.
+
+Errori: 
+1. riga 60, manca una virgola ;
+2. nella dichiarazione di gasolineCars errore di sintassi, l'arrow function scritta come un operatore >= ;
+3. nella dichiarazione di dieselCars sintassi errata, scritta come un'arrow function che non ritorna nessun valore. filter si aspetta un valore booleano di ritorno;
+4. nella dichiarazione di otherCards, auto.type !== 'benzina' || auto.type !== 'diesel'; è una condizione sbagliata perché esclude sia benzina che diesel. Dovrebbe essere && invece di ||; 
+5. c'è una { extra all'inizio dell'arrow function nella dichiarazione di otherCars: ci dovrebbe essere del codice all'interno che ritorna un valore.
+6. Nella dichiarazione dell'array, alcuni type sono scritti con lettera lowercase oppure uppercase; i type che iniziano con uppercase ("Benzina") non vengono presi nella condizione. corretto con auto.type.toLowerCase();
+*/
